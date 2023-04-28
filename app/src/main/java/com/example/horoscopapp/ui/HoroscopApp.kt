0862964259ac81ecd0c0d8fd60c33e0065fe5a18
Rodolfo Figueroa <ficcr23@hotmail.com>
2023-04-27 22:41:17 -0600

    implemented dagger hilt

---
+++ app/src/main/java/com/example/horoscopapp/ui/HoroscopApp.kt
@@ -0,0 +1,7 @@
+package com.example.horoscopapp.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HoroscopApp:Application()
---
+++ app/src/main/java/com/example/horoscopapp/ui/list/ListViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.horoscopapp.ui.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+//inyectarlo
+@HiltViewModel
+class ListViewModel @Inject constructor() :ViewModel() {
+}
